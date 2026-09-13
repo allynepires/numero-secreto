1bd34e948c8dc3eebe9f17cf5d598fb7814a4def
Allyne Pires <allynepires0310@gmail.com>
2026-09-13 17:42:00 -0300

    frase ajustada

--- app.js
+++ app.js
@@ -26,9 +26,9 @@ function verificarChute() {
         document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
         if (chute > numeroSecreto) {
-            exibirTextoNaTela('p', 'O número secreto é menor que o número informado.');
+            exibirTextoNaTela('p', 'O número secreto é menor.');
         } else {
-            exibirTextoNaTela('p', 'O número secreto é maior que o número informado.');
+            exibirTextoNaTela('p', 'O número secreto é maior.');
         }
         tentativas++;
         limparCampo();
